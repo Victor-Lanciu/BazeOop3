9b363d03835b111a0895db01df9845228d077a6a
Victor Lanciu <wlanciu@gmail.com>
2024-12-22 22:49:45 +0200

    Added first problem

---
+++ Problema1/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema1
+{
+    public static class Helper
+    {
+        public static string GenerareCodUnic(string prefix)
+        {
+            Random rand = new Random();
+            int randomCode = rand.Next();
+            return $"{prefix} {randomCode}";
+        }
+    }
+}
---
+++ Problema1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Problema1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstNum = Helper.GenerareCodUnic("373");
+            Console.WriteLine(firstNum);
+            string secondNum = Helper.GenerareCodUnic("112");
+            Console.WriteLine(secondNum);
+            string thirdNum = Helper.GenerareCodUnic("123");
+            Console.WriteLine(thirdNum);
+
+        }
+    }
+}
