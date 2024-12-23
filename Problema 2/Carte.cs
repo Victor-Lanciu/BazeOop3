a53eb83973dd3920ee0c746d325dc06de85c9d00
Victor Lanciu <wlanciu@gmail.com>
2024-12-23 12:01:39 +0200

    Added second problem.

---
+++ Problema 2/Carte.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_2
+{
+    public class Carte
+    {
+        public string Titlu { get; set; }
+        public string Autor { get; set; }
+        public int AnPublicare { get; set; }
+        public int NrPagini { get; set; }
+
+        public Carte(string titlu, string autor, int anPublicare, int nrPagini)
+        {
+            Titlu = titlu;
+            Autor = autor;
+            AnPublicare = anPublicare;
+            NrPagini = nrPagini;    
+        }
+        public void AfisareDetalii()
+        {
+            Console.WriteLine($"Titlul cartii: {Titlu}, Autorul: {Autor}, Anul de publicare: {AnPublicare}, " +
+                $"Numarul de pagini{NrPagini} ");
+        }
+    }
+}
---
+++ Problema 2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Problema_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carte cartea1 = new Carte("Learn c# in one day", "Jamie Chan", 2017, 200);
+            Carte cartea2 = new Carte("Behave", "Robert Sapolsky", 2018, 600);
+            Carte cartea3 = new Carte("Clean Code", "Robert Martin", 2008, 400);
+            cartea1.AfisareDetalii();
+            cartea2.AfisareDetalii();
+            cartea3.AfisareDetalii();
+        }
+    }
+}
