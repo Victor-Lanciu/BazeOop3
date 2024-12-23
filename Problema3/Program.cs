8b3da0e0ab4438a3d4c212994e57eca79f96f129
Victor Lanciu <wlanciu@gmail.com>
2024-12-23 12:15:00 +0200

    Added third problem

---
+++ Problema3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Problema3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Student student1 = new Student("Gabriel", 22, "Matematica si Informatica");
+            Student student1 = new Student("Igor", 23, " Informatica");
+            Student student2 = new Student(student1);
+            student1.AfisareDetalii();
+            student2.AfisareDetalii();
+            
+        }
+    }
+}
---
+++ Problema3/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema3
+{
+    public class Student
+    {
+        public string Nume { get; set; }
+        public int Varsta { get; set; }
+        public string Specializarea { get; set; }
+
+        public Student(string nume, int varsta, string specializarea)
+        {
+            Nume = nume;
+            Varsta = varsta;
+            Specializarea = specializarea;
+        }
+
+        public Student(Student studentCopy)
+        {
+            Nume = studentCopy.Nume;
+            Varsta = studentCopy.Varsta;
+            Specializarea = studentCopy.Specializarea;
+        }
+        public void AfisareDetalii()
+        {
+            Console.WriteLine($"Numele: {Nume}, Varsta: {Varsta}, Specializarea: {Specializarea}");
+        }
+
+    }
+}
